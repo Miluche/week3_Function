dfe954d910faeec2f00ffd4322de7ba302a34029
Miluche <118877950+Miluche@users.noreply.github.com>
2025-10-06 16:08:44 +0300

    function.py

--- function.py
+++ function.py
@@ -7,11 +7,11 @@ def calculate_discount(price, discount_percent):
         return price
 
 price = float(input("Enter the original price of the item: "))
-discount_percent = float(input("Enter the discount percentage: "))
+discount_percent = float(input("Enter the discount in percentage: "))
 
 final_price = calculate_discount(price, discount_percent)
 
 if discount_percent >= 20:
-    print(f"The final price after applying the discount is: ${final_price:.2f}")
+    print(f"The final price after the discount is: ${final_price:.2f}")
 else:
     print(f"No discount applied. The original price is: ${final_price:.2f}")
